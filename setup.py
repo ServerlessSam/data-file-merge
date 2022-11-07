3ae8dc2f77a737fbd42b38e4960b0077f587a209
ServerlessSam <samuel.t.lock@gmail.com>
2022-11-07 12:04:01 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,22 @@
-from setuptools import setup, find_packages
-
 # read the contents of your README file
 from pathlib import Path
+
+from setuptools import find_packages, setup
+
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 setup(
-    name='data-file-merge',
-    version='v0.2.2',
-    license='MIT',
+    name="data-file-merge",
+    version="v0.2.2",
+    license="MIT",
     author="Samuel Lock",
-    author_email='serverlesssam@gmail.com',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    url='https://github.com/ServerlessSam/data-file-merge',
-    keywords='configuration management merge split yaml json data files',
+    author_email="serverlesssam@gmail.com",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    url="https://github.com/ServerlessSam/data-file-merge",
+    keywords="configuration management merge split yaml json data files",
     description="Config-driven merging and splitting of JSON data files.",
     long_description=long_description,
-    long_description_content_type='text/markdown'
+    long_description_content_type="text/markdown",
 )
