4bc6099d44c432d46d6afd24829773810451fb47
ServerlessSam <samuel.t.lock@gmail.com>
2024-02-22 20:12:31 +0000

    Skeleton of split class

--- src/dfm/config.py
+++ src/dfm/config.py
@@ -191,3 +191,104 @@ class BuildConfig:
         if save_to_local_file:
             self.write_content(content)
         return content
+
+@dataclass
+class SplitConfig: 
+    source_file: SourceFile
+    destination_files: List[DestinationFile]
+    root_path: Path
+
+    def generate_new_dest_content(self) -> dict or List:
+        """
+        Synopsis:   Combines the current state of the desination file with desired source file content
+        Returns:    The new destination file content. Note the file has not been saved to disk yet.
+        """
+        dest_content = self.destination_file.content
+        for src in self.source_files:
+            jsonpath_expr = parse(src.destination_node)
+            dest_content_matches = [
+                match.value for match in jsonpath_expr.find(dest_content)
+            ]
+            if dest_content_matches == []:
+                dest_content_matches = [None]
+            for destination_match in dest_content_matches:
+                for src_content in src.retrieved_src_content:
+                    dest_json_merger = JsonMergerFactory(
+                        destination_match
+                    ).generate_json_merger()
+                    dest_json_merger.merge_obj(src_content)
+                    destination_match = dest_json_merger.json_obj
+                    dest_content = jsonpath_expr.update_or_create(
+                        dest_content, dest_json_merger.json_obj
+                    )
+        return dest_content
+
+    def write_content(self, content: dict):
+        # Only current use case is writing a destination file which at the moment uses the substituted path instead of a resolved path.
+        # This is because the file to write to can be new so doesn't resolve (hence have empty list for resolved_paths)
+        # JsonFileType.save_to_file(content, self.destination_file.destination_file_location.resolved_paths[0])
+        JsonFileType.save_to_file(
+            content,
+            self.root_path / self.destination_file.location.substituted_path,
+        )
+
+    @staticmethod
+    def load_config_from_file(
+        file_path: Path,
+        root_path: Path,
+        parameters=None,
+    ):
+        if parameters is None:
+            parameters = {}
+        config_dict = JsonFileType.load_from_file(file_path)
+        source_files = []
+        for src in config_dict["SourceFiles"]:
+            subs = {}
+            if "PathSubs" in src["SourceFileLocation"]:
+                for sub_key, sub_dict in src["SourceFileLocation"]["PathSubs"].items():
+                    reference_type = ReferenceTypeFactory(sub_dict["Type"]).generate()
+                    regex = RegexExtractor.parse_from_sub_dict(sub_dict)
+                    subs[sub_key] = Substitution(
+                        reference_type(
+                            parameters, None  # TODO support reading from content
+                        ),
+                        sub_dict["Value"],
+                        regex,
+                    )
+            source_files.append(
+                SourceFile(
+                    FileLocation(src["SourceFileLocation"]["Path"], root_path, subs),
+                    src["SourceFileNode"],
+                    src["DestinationFileNode"],
+                )
+            )
+        dest_subs = {}
+        if "PathSubs" in config_dict["DestinationFile"]["DestinationFileLocation"]:
+            for sub_key, sub_dict in config_dict["DestinationFile"][
+                "DestinationFileLocation"
+            ]["PathSubs"].items():
+                reference_type = ReferenceTypeFactory(sub_dict["Type"]).generate()
+                regex = RegexExtractor.parse_from_sub_dict(sub_dict)
+                dest_subs[sub_key] = Substitution(
+                    reference_type(
+                        parameters, None
+                    ),  # TODO support reading from content
+                    sub_dict["Value"],
+                    regex,
+                )
+        dest_file = DestinationFile(
+            FileLocation(
+                config_dict["DestinationFile"]["DestinationFileLocation"]["Path"],
+                root_path,
+                dest_subs,
+            )
+        )
+        return BuildConfig(
+            source_files=source_files, destination_file=dest_file, root_path=root_path
+        )
+    
+    def split(self, save_to_local_file: bool = True):
+        content = self.generate_new_dest_content()
+        if save_to_local_file:
+            self.write_content(content)
+        return content
